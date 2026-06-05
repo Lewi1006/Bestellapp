bcdc99c8381ced2cdb5bc0857fc3777851fd19bc
Lewi1006 <coach.wind11@gmail.com>
2026-06-05 15:23:01 +0200

    feat: local storage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function init() {
+  getFromLocalStorage();
   renderMenu();
   renderBasket();
+  
 }
 
 // #region render function
@@ -46,15 +48,16 @@ function renderDishes(indexMenu) {
 
 function renderBasket() {
   const basketRef = document.getElementById(`basket`);
-  // const totals = calculateTotal();
+
   // basket with elements that only need rendering once
   basketRef.innerHTML = getBasketTemplate();
 
   // render dish cards into getBasketTemplate
   // loop through items in basket with indexBasket
+  // render all elements per dish --> basket itself, count, price
+  // and totals outside loop because it is always there for the entire basket not just for the cards
   const basketWrapperRef = document.getElementById(`basket-wrapper`);
   basketWrapperRef.innerHTML = "";
-  
 
   for (let indexBasket = 0; indexBasket < basket.length; indexBasket++) {
     basketWrapperRef.innerHTML += getBasketCardTemplate(indexBasket);
@@ -63,6 +66,8 @@ function renderBasket() {
   }
 
   renderTotals();
+  renderCartCount();
+  saveToLocalStorage();
 }
 
 // dynamic content (count, price) in cards needs to be rendered individually
@@ -79,20 +84,22 @@ function renderPrice(indexBasket) {
   priceRef.innerHTML = total.toFixed(2).replace(".", ",");
 }
 
-function renderTotals(){
-   const totals = calculateTotal();
+// Render totals because they depend on the entire basket
+// --> not on a single item, so they must be recalculated after any change
+function renderTotals() {
+  const totals = calculateTotal();
 
   const subtotalPriceRef = document.getElementById(`subtotal-price`);
   subtotalPriceRef.innerHTML = `${totals.subtotal.toFixed(2).replace(".", ",")}€`;
-  
+
   const deliveryFeePriceRef = document.getElementById(`delivery-fee-price`);
   deliveryFeePriceRef.innerHTML = `${totals.deliveryFee.toFixed(2).replace(".", ",")}€`;
-  
+
   const totalPriceRef = document.getElementById(`total-price`);
-  totalPriceRef.innerHTML = `${totals.total.toFixed(2).replace(".", ",")}€`; 
+  totalPriceRef.innerHTML = `${totals.total.toFixed(2).replace(".", ",")}€`;
 
   const buyTotalref = document.getElementById(`buy-total`);
-  buyTotalref.innerHTML= `Buy now (${totals.total.toFixed(2).replace(".", ",")}€)`;
+  buyTotalref.innerHTML = `Buy now (${totals.total.toFixed(2).replace(".", ",")}€)`;
 }
 
 // renders the counter on shopping cart icon in mobile version
@@ -128,6 +135,10 @@ function renderCartCount() {
 // if the item is found = true the count of repeatingDish/basket[basketItem].count
 // --> in basketCartTemplate increases
 // else if item is not found we add it to the array by pushing it as an object
+// we create variable indexBasket so that the value of index from loop can be
+// --> passed into renderCount and renderPrice funtion
+// if dish already exist we only update count and price
+// --> else we add dish to array and render basket
 function addToBasket(indexMenu, indexDishes) {
   const dish = menu[indexMenu].dishes[indexDishes];
 
@@ -149,6 +160,7 @@ function addToBasket(indexMenu, indexDishes) {
     renderCount(indexBasket);
     renderPrice(indexBasket);
     renderTotals();
+    saveToLocalStorage();
   } else {
     basket.push({
       name: dish.name,
@@ -157,8 +169,9 @@ function addToBasket(indexMenu, indexDishes) {
     });
 
     renderBasket();
+    saveToLocalStorage();
   }
-  
+
   renderCartCount();
 }
 
@@ -166,6 +179,8 @@ function addToBasket(indexMenu, indexDishes) {
 // Called from getBasketCardTemplate via onclick
 // If the count reaches 0 or less, the item is removed from the basket array
 // After updating state, UI is re-rendered
+// if basket is empty render the entire basket
+// else just update count, price and totals
 function decreaseCount(indexBasket) {
   basket[indexBasket].count--;
 
@@ -173,6 +188,7 @@ function decreaseCount(indexBasket) {
     basket.splice(indexBasket, 1);
     renderBasket();
     renderCartCount();
+    saveToLocalStorage();
     return;
   }
 
@@ -180,9 +196,11 @@ function decreaseCount(indexBasket) {
   renderPrice(indexBasket);
   renderTotals();
   renderCartCount();
+  saveToLocalStorage();
 }
 
 // Increases the quantity of basket item by 1
+// just update count, price and totals
 function increaseCount(indexBasket) {
   basket[indexBasket].count++;
 
@@ -190,14 +208,18 @@ function increaseCount(indexBasket) {
   renderPrice(indexBasket);
   renderTotals();
   renderCartCount();
+  saveToLocalStorage();
 }
 
 // deletes item from basket array
 // called from getBasketCardTemplate via onclick
+// when deleting (splicing) whole basket needs to be rendered
+// --> cause no cards in there anymore
 function deleteFromBasket(indexBasket) {
   basket.splice(indexBasket, 1);
   renderBasket();
   renderCartCount();
+  saveToLocalStorage();
 }
 
 // calculates the prices of dishes
@@ -296,6 +318,22 @@ function closeDialog() {
 
   renderBasket();
   renderCartCount();
+}
+
+// #endregion
+
+// #region LOCAL STORAGE
+function saveToLocalStorage() {
+  localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+function getFromLocalStorage() {
+  const storedBasket = localStorage.getItem("basket");
+  let myArrBasket = JSON.parse(storedBasket);
+
+  if (myArrBasket !== null) {
+    basket = myArrBasket;
+  }
 }
 
 // #endregion
